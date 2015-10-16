ca8fa1af4a41fad28c69034e4ded25d8053ee781
ck <ck@kchen.cn>
2015-10-16 13:48:15 +0800

    基本实现自定义菜单的编辑和删除部分

--- static/js/index.js
+++ static/js/index.js
@@ -2,15 +2,89 @@
     
     var app = angular.module('weixin_menu', []);
     
+    /*app.directive('setFocus', function(){
+        return function(scope, element){
+            alert("ok");
+            element[0].focus();
+        };
+    });*/
+    /*app.directive('focus', function () {
+        return function (scope, element, attrs) {
+            attrs.$observe('focus', function (newValue) {
+                newValue === 'true' && element[0].focus();
+            });
+        }
+    });*/
+
+    function getLength(str) {
+        return str.replace(/[^\x00-\xff]/g,"aa").length;
+    };
+
     // 菜单编辑
     app.controller('menu', function($scope, $http) {
-        $scope.del = function(){
-            alert("del");
+        $scope.checked = [];
+        $scope.back = [];
+        $scope.has_error = false;
+
+        $scope.del = function(num) {
+            var lists = [], cnt = 0, flag = false;
+            var rank = $scope.lists[num]['rank'];
+            for (var i = 0; i < $scope.lists.length; i++) {
+                if (i != num) {
+                    if (rank==1 && flag && $scope.lists[i]['rank']!=1) {
+                        continue;
+                    }
+                    lists[cnt++] = $scope.lists[i];
+                    flag = false;
+                } else {
+                    flag = true;
+                }
+            };
+            for (var i = 0; i < lists.length; i++) {
+                lists[i].cnt = i;
+            };
+            //console.log(lists);
+            $scope.lists = lists;
         };
-        return $http({
+        $scope.myBlur = function(cnt) {
+            var name = $scope.lists[cnt].name.trim();
+            var rank = $scope.lists[cnt].rank;
+            $scope.has_error = false;
+            $scope.checked[cnt] = false;
+            if (name === "") {
+                $scope.lists[cnt].name = $scope.back[cnt];
+            }
+            while(getLength(name) > rank * 8) {
+                $scope.lists[cnt].name = name.substr(0, name.length-1);
+                name = $scope.lists[cnt].name;
+            }
+        }
+        $scope.edit = function(cnt) {
+            var rank = $scope.lists[cnt].rank;
+            if (rank == 3) {
+                return false;
+            }
+            $scope.back[cnt] = $scope.lists[cnt].name;
+            $scope.checked[cnt] = true;
+            setTimeout(function(){$(".input-" + rank).focus();}, 50);
+        };
+        $scope.myKeyup = function(e, cnt){
+            var name = $scope.lists[cnt].name.trim();
+            var rank = $scope.lists[cnt].rank;
+            if (getLength(name) > 8 * rank) {
+                $scope.has_error = true;
+            } else {
+                $scope.has_error = false;
+            }
+            var keycode = window.event ? e.keyCode : e.which;
+            if(keycode == 13) {
+                $scope.myBlur(cnt);
+            }
+        };
+        $http({
             url:'menu.json',
             method:'GET'
-        }).success(function(res){
+        }).success(function(res) {
             var lists = [], cnt = 0;
             for (var i = 0; i < res['button'].length; i++) {
                 var menu = res['button'][i];
@@ -22,20 +96,22 @@
                         lists[cnt][key] = menu[key];
                     }
                 }
+                lists[cnt]['cnt'] = cnt;
                 cnt++;
                 if (menu["sub_button"]) {
                     for (var j = 0; j < menu['sub_button'].length; j++) {
                         lists[cnt] = menu['sub_button'][j];
                         lists[cnt]['rank'] = 2;
+                        lists[cnt]['cnt'] = cnt;
                         cnt++;
                     };
                 }
-                if (!menu['sub_button'] || menu['sub_button'].length != 5) {
-                    lists[cnt++] = {
-                        name: "添加二级菜单",
-                        rank: 3
-                    }
-                }
+                lists[cnt] = {
+                    name: "添加二级菜单",
+                    rank: 3,
+                    cnt: cnt
+                };
+                cnt++;
             };
             //console.log(lists);
             $scope.lists = lists;
@@ -57,4 +133,5 @@
 
     });
 
+    $(".container").show();
 })();
